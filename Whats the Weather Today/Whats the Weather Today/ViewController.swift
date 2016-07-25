b9620cc245dd6ad6b58264e10b4492560ec352e5
shankar08 <shan08kar@gmail.com>
2016-07-24 21:25:19 -0500

    helo

--- Whats the Weather Today/Whats the Weather Today/ViewController.swift
+++ Whats the Weather Today/Whats the Weather Today/ViewController.swift
@@ -10,14 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var CityTextInput: UITextField!
+    @IBAction func FindWeather(sender: AnyObject) {
+        
+        
+        var wasSuccessful = false
+
+        let url = NSURL(string:"http://www.weather-forecast.com/locations/" + CityTextInput.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest" )!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            //checking if we have some data
+            //if we have data then assign it to urlContent
+            if let urlContent = data{
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if websiteArray?.count > 1{
+                    //   print(websiteArray![1])
+                    
+                    let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1{
+                        
+                        wasSuccessful = true
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.resultLabel.text = weatherSummary
+                        })
+                        
+                    }
+                    
+                }
+               
+                
+            }
+           
+            
+            
+        }
+        
+        task.resume()
+        if wasSuccessful == false{
+            self.resultLabel.text = "Couldnot find the weather for that city - please try again."
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(resultLabel: UITextField!) -> Bool
+    {
+        resultLabel.resignFirstResponder()
+        return true
     }
 
 
